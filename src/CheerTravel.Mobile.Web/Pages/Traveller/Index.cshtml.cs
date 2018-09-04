371c835d7ad9fd272b2e7ce26a69ce0d9eb6644f
frankdossing <frankdossing@gmail.com>
2018-09-04 07:07:42 +0200

    trying to fix issue with deployment to azure

--- src/CheerTravel.Mobile.Web/Pages/Traveller/Index.cshtml.cs
+++ src/CheerTravel.Mobile.Web/Pages/Traveller/Index.cshtml.cs
@@ -8,24 +8,31 @@ using CheerTravel.Mobile.Web.Interfaces;
 using CheerTravel.Mobile.Web.Helpers;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
 
 namespace CheerTravel.Mobile.Web.Pages.Traveller {
     public class IndexModel : PageModel
     {
         private readonly ISecurityManager _securityManager;
         private readonly IDapperUnitOfWork _dapperUnitOfWork;
+        private readonly ILogger<IndexModel> _logger;
 
-        public IndexModel(ISecurityManager securityManager, IDapperUnitOfWork travellerRep) {
+        public IndexModel(ISecurityManager securityManager, IDapperUnitOfWork travellerRep, ILogger<IndexModel> logger) {
             _securityManager = securityManager;
             _dapperUnitOfWork = travellerRep;
+            _logger = logger;
+            _logger.LogDebug("Constructor started up on Traveller Page");
         }
 
+        [BindProperty]
         public Data.Traveller LosPageModel {get;set;}
 
         public void OnGet()
         {
+            _logger.LogDebug("On get invoked");
             int userId = _securityManager.GetLoggedOnTravellerId(User.Identity.Name);
-            LosPageModel = _dapperUnitOfWork.TravellerRepository.Find(userId);
+            //LosPageModel = _dapperUnitOfWork.TravellerRepository.Find(userId);
+            LosPageModel = null;
         }
    }
 }
